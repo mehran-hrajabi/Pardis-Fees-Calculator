2afa0bb5357f2ad4106e4757ace12fe6c19a7ec5
Mehran Hajirajabi <mehran.hrajabi98@gmail.com>
2020-10-02 04:41:16 +0330

    Final version
    All seems to be finished. Yet minor problems in logic unit implementation are unsolved.

--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@ function calculator(){
 	
 	var sabet = 4312224 ;
 	var nazari = [] , amali = [] , karamoozi = [] , vahedNazari = [] , vahedAmali = [] , i 
-	, feeNazari = [] , feeAmali = [] , fee = [] , check = [] , karamooz ;
+	, feeNazari = [] , feeAmali = [] , fee = [] , check = [] , daneshkh = [] , temp , fix , pjt , fs ;
 	
 	/*Shahrie har vahed be ezaye har term az 1 ta 10*/
 	nazari[0] = 287481 ;
@@ -71,15 +71,40 @@ function calculator(){
 	check[7] = document.getElementById("karamoozi8").checked;
 	check[8] = document.getElementById("karamoozi9").checked;
 	check[9] = document.getElementById("karamoozi10").checked;
+
+	/*Danesh khanevade*/
+	daneshkh[0] = document.getElementById("danesh1").checked;
+	daneshkh[1] = document.getElementById("danesh2").checked;
+	daneshkh[2] = document.getElementById("danesh3").checked;
+	daneshkh[3] = document.getElementById("danesh4").checked;
+	daneshkh[4] = document.getElementById("danesh5").checked;
+	daneshkh[5] = document.getElementById("danesh6").checked;
+	daneshkh[6] = document.getElementById("danesh7").checked;
+	daneshkh[7] = document.getElementById("danesh8").checked;
+	daneshkh[8] = document.getElementById("danesh9").checked;
+	daneshkh[9] = document.getElementById("danesh10").checked;
 	
 	/*karamoozi*/
-	karamooz=0 ;
 	for(i=0 ; i<check.length ; i++){
 		if(check[i] == true){
-			karamooz = karamoozi[i];
+			temp = i ;
 			break;
 		}
 	}
+
+	/*danesh khanevadeh*/
+	for(i=0 ; i<daneshkh.length ; i++){
+		if(daneshkh[i] == true){
+			fs = i ;
+			break;
+		}
+	}
+
+	/*Tamdide project*/
+	pjt = document.getElementById("tamdid").value ;
+	pjt-- ;
+
+	if(pjt>10){pjt = 10 ;}
 	
 	/*Mohasebe nazari va amali har term*/
 	for(i=0 ; i<vahedNazari.length ; i++){
@@ -91,27 +116,98 @@ function calculator(){
 	
 	/*Jamee nazari va amali har term*/
 	for(i=0 ; i<nazari.length ; i++){
-		fee[i] = feeAmali[i] + feeNazari[i] ;
+		if(i == temp){
+			fee[i] = feeAmali[i] + feeNazari[i] + karamoozi[i] ;
+		}
+		else{
+			fee[i] = feeAmali[i] + feeNazari[i] ;
+		}
+		if(i == pjt){
+			fee[i] += karamoozi[i] ;
+		}
+		if(i == fs){
+			fee[i] += karamoozi[i] ;
+		}
 	}
+	document.getElementById("mot1").innerHTML = fee[0] ;
+	document.getElementById("mot2").innerHTML = fee[1] ;
+	document.getElementById("mot3").innerHTML = fee[2] ;
+	document.getElementById("mot4").innerHTML = fee[3] ;
+	document.getElementById("mot5").innerHTML = fee[4] ;
+	document.getElementById("mot6").innerHTML = fee[5] ;
+	document.getElementById("mot7").innerHTML = fee[6] ;
+	document.getElementById("mot8").innerHTML = fee[7] ;
+	document.getElementById("mot9").innerHTML = fee[8] ;
+	document.getElementById("mot10").innerHTML = fee[9] ;
 
-	/*Mohasebe shahrie sabet*/
+
+	/*Mohasebe shahrie sabet - Mitavan herfei tar nevesht*/
+	fix = sabet * 8 ;
+	if(vahedNazari[8] == 0 && vahedAmali[8] == 0){
+		document.getElementById("sab9").innerHTML = 0 ;
+		document.getElementById("kol9").innerHTML = 0 ;
+	}
+	else if(vahedNazari[8] != 0 || vahedAmali[8] != 0){
+		if(vahedAmali[8]+vahedNazari[8]<5){
+			fix += (sabet/2) ;
+			document.getElementById("sab9").innerHTML = 2156112 ;
+			document.getElementById("kol9").innerHTML = fee[8] + 2156112 ;
+			}
+		else{
+			fix += sabet ;
+			document.getElementById("sab9").innerHTML = 4312224 ;
+			document.getElementById("kol9").innerHTML = fee[8] + 4312224 ;
+
+		}
+		fix += sabet ;
+	}
 	if(vahedNazari[9] == 0 && vahedAmali[9] == 0){
-		sabet = 9 * sabet ;
+		document.getElementById("sab10").innerHTML = 0 ;
+		document.getElementById("kol10").innerHTML = 0 ;
+
 	}
-	else if(vahedNazari[8] == 0 && vahedAmali[8] == 0 && vahedNazari[9] == 0 && vahedAmali[9] == 0){
-		sabet = 8 * sabet ;
+	else if(vahedNazari[9] != 0 || vahedAmali[9] != 0){
+		if(vahedAmali[9]+vahedNazari[9]<5){
+		fix += (sabet/2) ;
+		document.getElementById("sab10").innerHTML = 2156112 ;
+		document.getElementById("kol10").innerHTML = fee[9] + 2156112 ;
+		}
+		else{
+			fix += sabet ;
+			document.getElementById("sab10").innerHTML = 4312224 ;
+			document.getElementById("kol10").innerHTML = fee[9] + 4312224 ;
+		}
 	}
-	else{
-		sabet = 10 * sabet ;
-	}
+	document.getElementById("sab1").innerHTML = 4312224 ;
+	document.getElementById("sab2").innerHTML = 4312224 ;
+	document.getElementById("sab3").innerHTML = 4312224 ;
+	document.getElementById("sab4").innerHTML = 4312224 ;
+	document.getElementById("sab5").innerHTML = 4312224 ;
+	document.getElementById("sab6").innerHTML = 4312224 ;
+	document.getElementById("sab7").innerHTML = 4312224 ;
+	document.getElementById("sab8").innerHTML = 4312224 ;
+	document.getElementById("fixed").innerHTML = fix ;
+
+	/*Jamee amali va nazari har term*/
+	document.getElementById("kol1").innerHTML = fee[0] + 4312224 ;
+	document.getElementById("kol2").innerHTML = fee[1] + 4312224 ;
+	document.getElementById("kol3").innerHTML = fee[2] + 4312224 ;
+	document.getElementById("kol4").innerHTML = fee[3] + 4312224 ;
+	document.getElementById("kol5").innerHTML = fee[4] + 4312224 ;
+	document.getElementById("kol6").innerHTML = fee[5] + 4312224 ;
+	document.getElementById("kol7").innerHTML = fee[6] + 4312224 ;
+	document.getElementById("kol8").innerHTML = fee[7] + 4312224 ;
+
 
 	/*Jamee kol*/
 	total=0 ;
 	for(i=0 ; i<fee.length ; i++){
 		total += fee[i] ;
 	}
-	total = total + karamooz + sabet ;
+	document.getElementById("mottot").innerHTML = total ;
+	total = total + fix ;
+	document.getElementById("tot").innerHTML = total ;
 	
-	window.alert(total);
+	/*window.alert(total);*/
 
 }
